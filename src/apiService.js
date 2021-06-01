bfcda074640a3c3a1bf6483d489a50885886f328
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-06-01 10:58:30 +0300

    save 01/06

---
+++ src/apiService.js
@@ -0,0 +1,31 @@
+const PAGE_KEY = '21816911-1420e6eb818d750af174e21f8';
+export default class CardApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    
+    fetchArticles() {
+        return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${PAGE_KEY}`)
+            .then(r => r.json())
+            .then(data => {
+                this.page += 1;
+                return data.hits;
+            })
+            .catch(error =>
+                console.log('error', error));
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
+
